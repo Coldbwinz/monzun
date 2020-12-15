d2c0c084639bcf22fe4562a9feb0dd15caa07d0f
Coldbwinz <whentheendisgettingcloser@mail.ru>
2020-12-15 14:01:25 +0300

    user entity

---
+++ src/main/java/com/example/monzun/entities/PasswordResetToken.java
@@ -0,0 +1,30 @@
+package com.example.monzun.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "password_reset_tokens", schema = "public")
+public class PasswordResetToken {
+    @Id
+    @Column(name = "token_id", updatable = false, nullable = false)
+    @SequenceGenerator(name = "pr_tokens_seq",
+            sequenceName = "password_reset_tokens_token_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "pr_tokens_seq")
+    private Long id;
+    @Column(name = "token")
+    private String token;
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @Column(name = "expired_at")
+    private LocalDateTime expiredAt = LocalDateTime.now().plusMinutes(2880); //2 days
+}
---
+++ src/main/java/com/example/monzun/entities/Role.java
@@ -0,0 +1,44 @@
+package com.example.monzun.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles", schema = "public")
+public class Role {
+
+    @Id
+    @Column(name = "role_id", updatable = false, nullable = false)
+    @SequenceGenerator(name = "roles_seq",
+            sequenceName = "roles_role_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "roles_seq")
+    private int roleId;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "title")
+    private String title;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return roleId == role.roleId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleId);
+    }
+}
