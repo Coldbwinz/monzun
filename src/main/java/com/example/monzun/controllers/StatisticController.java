dd82140478df06c4d3482ca5904d7ae26dd237e0
Coldbwinz <whentheendisgettingcloser@mail.ru>
2020-12-24 20:54:02 +0300

    startup in tracking stats

---
+++ src/main/java/com/example/monzun/controllers/StatisticController.java
@@ -0,0 +1,40 @@
+package com.example.monzun.controllers;
+
+
+import com.example.monzun.exception.NoAuthUserException;
+import com.example.monzun.services.StatisticService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityNotFoundException;
+
+@Validated
+@RestController
+@RequestMapping("/api/stats")
+public class StatisticController extends BaseRestController {
+
+    private final StatisticService statisticService;
+
+    public StatisticController(StatisticService statisticService) {
+        this.statisticService = statisticService;
+    }
+
+    @GetMapping("/{trackingId}/{startupId}")
+    public ResponseEntity<?> getStats(@PathVariable Long trackingId, @PathVariable Long startupId) {
+        try {
+            return ResponseEntity.ok(statisticService.get(trackingId, startupId, getAuthUser()));
+        } catch (NoAuthUserException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(getErrorMessage("not_found", e.getMessage()));
+        } catch (AccessDeniedException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(getErrorMessage("forbidden", e.getMessage()));
+        }
+    }
+}
