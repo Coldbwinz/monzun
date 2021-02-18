8f5e2b532a455e3d4d1a1460182d8ec91229f7a3
Zaznatov <eugene.zaznatov@yandex.ru>
2021-02-18 22:27:53 +0300

    fix new password valid

--- src/main/java/com/example/monzun/requests/PasswordChangeRequest.java
+++ src/main/java/com/example/monzun/requests/PasswordChangeRequest.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Getter
@@ -17,7 +16,6 @@ public class PasswordChangeRequest {
     @NotNull
     @ApiModelProperty(required = true)
     private String token;
-    @NotNull @Min(6)
-    @ApiModelProperty(required = true)
+    @NotNull @ApiModelProperty(required = true)
     private String newPassword;
 }
